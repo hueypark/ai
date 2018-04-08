591be27ab4b2a6ae889e13c0473b381def09c085
hueypark <jaewan.huey.park@gmail.com>
2018-04-08 22:16:51 +0900

    Seperate logic, render thread

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/hueypark/ai/ctx"
 	"github.com/hueypark/ai/renderer"
 	"github.com/hueypark/ai/world"
+	"sync"
 	"time"
 )
 
@@ -17,14 +18,30 @@ func main() {
 	w.AddActor(l)
 
 	lastUpdateTime := time.Now()
+	var mux sync.RWMutex
+	go func() {
+		ticker := time.NewTicker(time.Second / 60)
+		for {
+			<-ticker.C
+
+			func() {
+				mux.Lock()
+				defer mux.Unlock()
+
+				delta := time.Since(lastUpdateTime).Seconds()
+				lastUpdateTime = time.Now()
+
+				ctx.PhysicsWorld.Tick(delta)
+
+				w.Update(delta)
+			}()
+		}
+	}()
+
 	ebiten.Run(
 		func(screen *ebiten.Image) error {
-			delta := time.Since(lastUpdateTime).Seconds()
-			lastUpdateTime = time.Now()
-
-			ctx.PhysicsWorld.Tick(delta)
-
-			w.Update(delta)
+			mux.RLock()
+			defer mux.RUnlock()
 
 			for _, actor := range w.Actors() {
 				renderer.Render(screen, actor)
